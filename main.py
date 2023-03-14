4947ed40b382807fe913c87dd2b4113c6163addd
Kolawat Inpan <kolawat_@hotmail.com>
2023-03-14 17:16:13 +0700

    payment method

---
+++ main.py
@@ -0,0 +1,7 @@
+from payment import Payment
+
+payment_test_obj1 = Payment(500, "15-03-2023")
+payment_test_obj2 = Payment(1000, "16-03-2023")
+payment_test_obj3 = Payment(1500, "17-03-2023")
+payment_test_obj4 = Payment(2000, "18-03-2023")
+payment_test_obj4.make_payment()
