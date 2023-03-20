16bb6268b52b5bab66b22b122bbdd7262a76317f
Kolawat Inpan <dominolity@gmail.com>
2023-03-20 15:25:41 +0700

    account login

---
+++ comment.py
@@ -0,0 +1,4 @@
+class Comment():
+    def __init__(self, user, comment):
+        self.__user = user 
+        self.__comment = comment
---
+++ main_account.py
@@ -0,0 +1,4 @@
+from account import Account, User, Admin
+
+user1 = User('Name', 'pass','test@gmail.com','0903031788','Natdanai')
+print(user1.login())
