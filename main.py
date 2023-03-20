f90e2ab2b6a8063e975458937fa0915e3e01650b
Author Name <email@address.com>
2023-03-20 15:46:31 +0700

    account login

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from payment import Payment, CardPayment
+from account import Account, User, Admin
 
-payment_test_obj1 = Payment(500, "15-03-2023")
-payment_test_obj2 = Payment(1000, "16-03-2023")
-payment_test_obj3 = Payment(1500, "17-03-2023")
-payment_test_obj4 = Payment(2000, "18-03-2023")
-payment_test_obj4.make_payment()
-
-CreditCard1 = CardPayment(500, "18-03-2023", "Kolawat Inpan", "1234 4321 2562 2512", "01/28", 601)
-CreditCard1.make_payment()
+user1 = User('Name', 'pass','test@gmail.com','0903031788','Natdanai')
+print(user1.login())
--- main_account.py
+++
@@ -1,4 +0,0 @@
-from account import Account, User, Admin
-
-user1 = User('Name', 'pass','test@gmail.com','0903031788','Natdanai')
-print(user1.login())
