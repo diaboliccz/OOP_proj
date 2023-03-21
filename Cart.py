17ae71cb1285adea14b90111ee59f9def78418ff
boomoioi <64312642+boomoioi@users.noreply.github.com>
2023-03-21 16:07:45 +0700

    edit

--- Cart.py
+++ Cart.py
@@ -1,13 +1,12 @@
+import datetime
 class Cart():
     def __init__(self, payment):
         self.__room_list = []
-        self.__check_in_date_list = []
-        self.__check_out_date_list = []
         self.__total_price = 0
-        self.__status = False
+        self.__payment_status = False
         self.__payment = payment
         
-    def add_room(self, room, check_in_date, check_out_date):
+    def add_room(self, room):
         pass
 
     def checkout(self):
