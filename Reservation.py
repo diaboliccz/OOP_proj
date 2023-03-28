f03b264ebfa785d30a8b828a5e9b0aa1c0465dfc
j4ykitti <110811397+j4ykitti@users.noreply.github.com>
2023-03-28 16:30:44 +0700

    Add files via upload

--- Reservation.py
+++ Reservation.py
@@ -1,9 +1,22 @@
 from RoomReserved import *
-class Reservation():
+from RoomType import *
+from Hotel import *
+class Reservation(RoomReserved,RoomType,Hotel):
     def __init__(self, room_list, date):
         self.__room_reserved_list = room_list
         self.__date_pay = date
+        self.__hotel_name = Hotel.hotel_name
+       
 
+    
+    def booked_list(self):
+        temp = [self.__date_pay]
+        for room in self.__room_reserved_list:
+            temp.append([room.hotel.hotel_name, room.check_in_date, room.check_out_date, room.price, room.status])
+
+        return temp
+
+    
     @property
     def room_reserved_list(self):
         return self.__room_reserved_list
@@ -11,3 +24,7 @@ class Reservation():
     @property
     def date_pay(self):
         return self.__date_pay
+    
+
+    
+    
