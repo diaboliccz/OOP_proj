26c11a09d25e400bbaf6415f6bd3e2efe69c349a
Natdanai <frame.sk140@gmail.com>
2023-04-04 02:55:25 +0700

    FRAMEE17

--- comment.py
+++ comment.py
@@ -1,8 +1,26 @@
+import datetime
 class Comment():
-    def __init__(self, user, comment):
+    def __init__(self, user,hotel, comment,rating):
         self.__user = user 
+        self.__hotel = hotel
         self.__comment = comment
-    
+        self.__rating = rating
+        self.__comment_date = datetime.datetime.now()
+    @property
+    def user(self):
+        return self.__user
+    @property
+    def hotel(self):
+        return self.__hotel
+    @property
     def comment(self):
-        pass
+        return self.__comment
+    @property
+    def rating(self):
+        return self.__rating
+    @property
+    def comment_date(self):
+        return self.__comment_date
+    def __str__(self):
+        return f"{self.user.full_name} | ({self.__comment_date}) --> {self.__comment} | Rating : {self.__rating}"
     
